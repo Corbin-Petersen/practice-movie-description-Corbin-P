c9e67363a4f8cf4f631cd9a6af240c9459002d3a
Corbin-Petersen <corbincpetersen@gmail.com>
2025-07-07 15:42:14 -0500

    added api and gitignore call

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,13 +8,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws HttpException, IOException {
         Client client = new Client();
-
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a question for the model:");
-        String query = scanner.nextLine();
 
-        GenerateContentResponse response = client.models.generateContent("gemini-2.0-flash-001", query, null);
+        System.out.println("Enter Movie title:");
+        String title = scanner.nextLine();
+        System.out.println("Enter rating (1-5)");
+        int rating = scanner.nextInt();
 
-        System.out.println(response.text());
+        GenerateContentResponse description = client.models.generateContent("gemini-2.0-flash-001", title, null);
+
+        System.out.println(description.text());
     }
 }
